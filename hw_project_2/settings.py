78dfcad35220eb27151023c581d99449a6680ffb
Artur <archibay16@gmail.com>
2022-11-10 22:20:45 +0200

    add travis, flake8, requirements; create models and make migrations (+migrate); start to do managment command

--- hw_project_2/settings.py
+++ hw_project_2/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=rv4ta#aqh$dwpdfwvb8mx4_ve=!oco@-a^z&j4dh=mq5-7tgn'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-=rv4ta#aqh$dwpdfwvb8mx4_ve=!oco@-a^z&j4dh=mq5-7tgn')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'new_app'
 ]
 
 MIDDLEWARE = [
@@ -105,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Kiev'
 
 USE_I18N = True
 
